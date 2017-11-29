9552e1cf11b021a5a42a2b7c85a507f5ad6a74ad
darri19 <darri19@gmail.com>
2017-11-29 23:52:31 +0000

    Slightly prettyfies cell view, also adds poster to detail view

--- MovieSearch/HelloWorld/HelloWorld.iOS/Controllers/MovieDetailsViewController.cs
+++ MovieSearch/HelloWorld/HelloWorld.iOS/Controllers/MovieDetailsViewController.cs
@@ -14,14 +14,14 @@ namespace HelloWorld.iOS.Controllers
 	{
 		private Movie _movie { get; set; }
 
-		private const double StartX = 20;
+		private const double StartX = 5;
 		private const double StartY = 80;
 		private const double Height = 50;
 
-		public MovieDetailsViewController(Movie movie)
+        public MovieDetailsViewController(Movie movie)
 		{
 			this._movie = movie;
-		}
+        }
 
 		public override void ViewDidLoad()
 		{
@@ -29,17 +29,27 @@ namespace HelloWorld.iOS.Controllers
 			this.View.BackgroundColor = UIColor.White;
 			this.Title = "Movie info";
 
-			UILabel detailLabel = DetailLabel();
-			this.View.AddSubviews(new UIView[] { detailLabel });
+			UILabel titleLabel = TitleLabel();
+
+            UIImageView imageView = new UIImageView()
+            {
+                Frame = new CGRect(StartX, StartY + Height + 5, 40, 60),
+
+                Image = UIImage.FromFile(this._movie.ImageName)
+            };
+            this.View.AddSubviews(new UIView[] { titleLabel, imageView });
 		}
 
-		private UILabel DetailLabel()
+		private UILabel TitleLabel()
 		{
 			return new UILabel()
 			{
 				Frame = new CGRect(StartX, StartY, this.View.Bounds.Width - 2 * StartX, Height),
-				Text = _movie.Title
-			};
+				Text = _movie.Title + " (" + _movie.Year + ")",
+                Font = UIFont.FromName("ArialMT", 13f),
+                TextColor = UIColor.FromRGB(0, 0, 0),
+                BackgroundColor = UIColor.Clear
+            };
 		}
 	}
 }
--- MovieSearch/HelloWorld/HelloWorld.iOS/Views/MovieCell.cs
+++ MovieSearch/HelloWorld/HelloWorld.iOS/Views/MovieCell.cs
@@ -11,7 +11,7 @@ namespace MovieSearch.iOS.Views
 {
 	public class MovieCell : UITableViewCell
 	{
-		private const double ImageHeight = 55;
+		private const double ImageHeight = 33;
 		private UIImageView _imageView;
 		private UILabel _headingLabel;
 		private UILabel _subheadingLabel;
@@ -28,18 +28,17 @@ namespace MovieSearch.iOS.Views
 
 			this._headingLabel = new UILabel()
 			{
-				Frame = new CGRect(25, 5, this.ContentView.Bounds.Width - 60, 25),
-				Font = UIFont.FromName("ArialMT", 22f),
+				Frame = new CGRect(ImageHeight + 15, 5, this.ContentView.Bounds.Width - 60, 25),
+				Font = UIFont.FromName("ArialMT", 13f),
 				TextColor = UIColor.FromRGB(0, 0, 0),
 				BackgroundColor = UIColor.Clear
 			};
 
 			this._subheadingLabel = new UILabel()
 			{
-				Frame = new CGRect(100, 25, 100, 20),
-				Font = UIFont.FromName("AmericanTypewriter", 12f),
-				TextColor = UIColor.FromRGB(38, 127, 0),
-				TextAlignment = UITextAlignment.Center,
+				Frame = new CGRect(ImageHeight + 15, 22, this.ContentView.Bounds.Width - 60, 20),
+				Font = UIFont.FromName("ArialMT", 9f),
+				TextColor = UIColor.FromRGB(100, 100, 100),
 				BackgroundColor = UIColor.Clear
 			};
 
@@ -51,7 +50,7 @@ namespace MovieSearch.iOS.Views
 		public void UpdateCell(Movie movie)
 		{
 			this._imageView.Image = UIImage.FromFile(movie.ImageName);
-			this._headingLabel.Text = movie.Title + '(' + movie.Year + ')';
+			this._headingLabel.Text = movie.Title + " (" + movie.Year + ')';
 			string castMembers = "";
 			for(int i = 0; i < movie.Cast.Count && i < 3; i++)
 			{
